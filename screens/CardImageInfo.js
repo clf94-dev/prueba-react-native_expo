bc3703479f7024bd7b63efeb88e030edccd5e48d
clf94-dev <clf94@hotmail.es>
2021-02-12 09:42:30 +0100

    fixed data

--- screens/CardImageInfo.js
+++ screens/CardImageInfo.js
@@ -1,6 +1,6 @@
 export const CardImageInfo = [{
     index: 0,
-    title: 'beach',
+    title: 'Beach',
     text: 'His audiam deserunt in, eum ubique voluptatibus te. In reque dicta usu. Ne rebum dissentiet eam, vim omnis deseruisse id. Ullum deleniti vituperata at quo, insolens complectitur te eos, ea pri dico munere propriae. Vel ferri facilis ut, qui paulo ridens praesent ad. Possim alterum qui cu. Accusamus consulatu ius te, cu decore soleat appareat usu.',
     src: require('../assets/images/beach.jpg')
 },
@@ -15,4 +15,4 @@ export const CardImageInfo = [{
     title: 'Mountain',
     text: 'Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere corrumpit ut pri. Ea ipsum appareat erroribus mea. Mei probo inani aliquid ad. Omnis fabulas concludaturque an vix, an noluisse takimata facilisis pro, sit te volumus mandamus. Ad malorum imperdiet duo, ea possim utamur accusamus vix.',
     src: require('../assets/images/mountain.jpg')
-}]
+}];
